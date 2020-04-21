60d3dda5f4ab981b20f2f3c7fcf8cd95b2525f2f
nikoladjukic94 <59332199+nikoladjukic94@users.noreply.github.com>
2020-04-21 19:30:03 +0200

    Add files via upload

---
+++ theme/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("#content-slider").lightSlider({
+        loop: true,
+        item: 1,
+        auto: 3,
+        pause: 3000,
+        controls: false,
+        keyPress: true
+    });
+});
+
+$(document).ready(function (){
+    // all news pagination
+});
