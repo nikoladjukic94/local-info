39515c0495ee3cd5ac690812399ca078ba7817ec
nikoladjukic94 <59332199+nikoladjukic94@users.noreply.github.com>
2020-04-29 17:54:18 +0200

    Add files via upload

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,14 +1,25 @@
-$(document).ready(function () {
+$(document).ready(() => {
     $("#content-slider").lightSlider({
         loop: true,
         item: 1,
         auto: 3,
-        pause: 3000,
+        pause: 5000,
         controls: false,
         keyPress: true
     });
 });
-
-$(document).ready(function (){
-    // all news pagination
+$(document).ready(() => {
+    const x = $('#city-list');
+    const y = $('#show-cities');
+    var z = true;
+    y.click(() => {
+        if (z) {
+            x.show('slow');
+            z = false;
+        }
+        else {
+            x.hide('slow');
+            z = true;
+        }
+    });
 });
