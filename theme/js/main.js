5be46f13405a820dd19a3bd2fb7e56e5151c1ac4
nikoladjukic94 <59332199+nikoladjukic94@users.noreply.github.com>
2020-04-30 11:38:45 +0200

    Add files via upload

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,3 +1,4 @@
+//SLIDER
 $(document).ready(() => {
     $("#content-slider").lightSlider({
         loop: true,
@@ -8,6 +9,7 @@ $(document).ready(() => {
         keyPress: true
     });
 });
+//CITY LIST
 $(document).ready(() => {
     const x = $('#city-list');
     const y = $('#show-cities');
@@ -23,3 +25,40 @@ $(document).ready(() => {
         }
     });
 });
+//FORM VALIDATION
+const formValidation = () => {
+    let name = $('#fname').val();
+    let namemsg = $('#mname');
+    let phone = $('#fphone').val();
+    let phonemsg = $('#mphone');
+    let email = $('#femail').val();
+    let emailmsg = $('#memail');
+    let br = 0;
+    if (!(/^[a-zA-Z" "]+$/.test(name)) || name.length == 0 || !name.includes(" ")) {
+        namemsg.html("Neispravno ime");
+    }
+    else {
+        namemsg.html(" ");
+        br++;
+    }
+    if (!(/^[0-9"+"]+$/.test(phone)) || phone.length == 0 || phone.length != 12 || !phone.startsWith('+382')) {
+        phonemsg.html("Neispravan broj");
+    }
+    else {
+        phonemsg.html(" ");
+        br++;
+    }
+    if (!(/^[a-zA-Z"@._"]+$/.test(email)) || email.length == 0 || !email.includes("@") || !email.includes(".com")) {
+        emailmsg.html("Neispravan email");
+    }
+    else {
+        emailmsg.html(" ");
+        br++;
+    }
+    if (br == 3) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
